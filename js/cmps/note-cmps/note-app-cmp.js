1e761706e06924c58aec2d35b0581048ea802378
kareenb <kareenb89@gmail.com>
2018-07-05 21:01:14 +0300

    deleted folders not necessary

--- js/cmps/note-cmps/note-app-cmp.js
+++ js/cmps/note-cmps/note-app-cmp.js
@@ -1,10 +1,31 @@
 'use strict';
 
+import noteService from '../../services/note-service.js';
+import noteList from './note-list-cmp.js';
+
 export default {
     template: `
-        <section class="notes-app">
+        <section class="note-app">
             <button @click="$router.push('/')">Back to AppSus</button>
-            <h1>Hello Miss Notes</h1>
+            <h1>Miss Notes</h1>
+            <note-list :noteCmps="noteCmps"></note-list>
         </section>
-    `
+    `,
+
+    data() {
+        return {
+            noteCmps: []
+        }
+    },
+
+    created() {
+        noteService.query()
+            .then((noteCmps => {
+                this.noteCmps = noteCmps;
+            }))
+    },
+
+    components: {
+        noteList
+    }
 }
