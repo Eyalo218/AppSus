97dd589f837ce923d287d487c927380cd9e1d3ae
kareenb <kareenb89@gmail.com>
2018-07-06 21:27:13 +0300

    added nested routes to open note from note list; added closing note option; changed note ids to random

--- js/cmps/note-cmps/note-details-cmp.js
+++ js/cmps/note-cmps/note-details-cmp.js
@@ -1,24 +1,37 @@
 'use strict';
 
 import noteService from '../../services/note-service.js';
+import txtNote from './note-type-cmps/txt-note-cmp.js';
+import photoNote from './note-type-cmps/photo-note-cmp.js';
+import listNote from './note-type-cmps/list-note-cmp.js';
 
 export default {
     template: `
-        <section class="note-details">
-            <h2>{{note.title}}</h2>
+        <section class="note-details" v-if="noteCmp">
+            <button @click="$router.push('/missNotes')">X</button>
+            <component :is="noteCmp.cmpType" :data="noteCmp"></component>
         </section>
     `,
 
     data() {
         return {
-            note: null
+            noteCmp: null
         }
     },
 
     created() {
         noteService.getNoteById(this.$route.params.noteId)
             .then((note) => {
-                this.note = note;
+                this.noteCmp = note;
             })
+    },
+
+    methods: {
+    },
+
+    components: {
+        txtNote,
+        photoNote,
+        listNote
     }
 }
